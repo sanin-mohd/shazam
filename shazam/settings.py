00f6cb9b8fbd1a2f8f2fa3d4b7976dc7fcf858a3
sanin-mohd <sanin.nitcalicut.ece@gmail.com>
2021-12-13 17:38:02 +0530

    static files integrated with digital ocean spaces

--- shazam/settings.py
+++ shazam/settings.py
@@ -53,7 +53,7 @@ INSTALLED_APPS = [
     'banners',
     'offers',
     'coupons',
-    
+    'storages',
     
 
     
@@ -159,8 +159,18 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_ENDPOINT_URL = config('AWS_S3_ENDPOINT_URL')
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_LOCATION = 'AWS_LOCATION'
+DEFAULT_FILE_STORAGE='storages.backends.s3boto3.S3Boto3Storage'
 
-STATIC_URL = '/static/'
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_ENDPOINT_URL, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_DIRS=[BASE_DIR/'static']
 STATIC_ROOT=BASE_DIR/'assets'
 
