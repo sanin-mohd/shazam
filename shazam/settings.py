f29016c5aeae5d2284319efc7d337b07084820af
sanin-mohd <sanin.nitcalicut.ece@gmail.com>
2021-12-14 14:03:58 +0530

    bug fixed

--- shazam/settings.py
+++ shazam/settings.py
@@ -171,6 +171,7 @@ DEFAULT_FILE_STORAGE='storages.backends.s3boto3.S3Boto3Storage'
 
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_ENDPOINT_URL, AWS_LOCATION)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
 STATICFILES_DIRS=[BASE_DIR/'static']
 STATIC_ROOT=BASE_DIR/'assets'
 
